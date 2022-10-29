088bede22e4efa2ab9cb46d33dd58648d95cc61d
Eric Webber <ewebber283@gmail.com>
2022-10-29 14:29:09 -0500

    changed api

--- assets/script.js
+++ assets/script.js
@@ -11,18 +11,40 @@ const searchBtn = document.querySelector('#search-form')
 searchBtn.addEventListener('submit', clickSearch)
 
 
-
 apiRequest = (userInput) => {
-    fetch("https://ergast.com/api/f1/" + userInput + "/driverStandings.json").then(function(response) {
-        response.json().then(function(data) {
-            displayStandings(data)
-        });
-    });
-}
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'faf1df0ca0msh076ac77b5167b92p1214afjsn25e99da003c3',
+		'X-RapidAPI-Host': 'api-formula-1.p.rapidapi.com'
+	}
+};
 
-displayStandings = (year) => {
-    console.log(year)
-    standingsContainerEl.textContent = "";
-    standingsSearchTerm.textContent = year;
-}
+fetch('https://api-formula-1.p.rapidapi.com/rankings/drivers?season=' + userInput, options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
 
+	standingsContainerEl.textContent = "";
+	standingsSearchTerm.textContent = userInput;
+
+	for (let i = 0; i < response.length; i++) {
+		
+		const yearName = response[i].season;
+	  
+		
+		const standingsEl = document.createElement("div");
+		standingsEl.classList = "list-item flex-row justify-space-between align-center";
+	  
+	
+		const titleEl = document.createElement("span");
+		titleEl.textContent = yearName;
+	  
+		
+		standingsEl.appendChild(titleEl);
+	  
+		
+		standingsContainerEl.appendChild(standingsEl);
+	  }
+
+}
